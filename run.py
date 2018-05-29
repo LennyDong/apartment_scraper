a040e338d7040c930d7ceb42c684a11e833f09bf
Lenny Dong <dong.lenny@gmail.com>
2018-05-29 03:09:08 -0700

    hello

--- run.py
+++ run.py
@@ -18,7 +18,10 @@ RECEIPIENTS = ['dong.lenny@gmail.com', 'michael.chen@berkeley.edu', 'acdelapaz@b
 
 def get_avalon():
     listings = []
-    driver = webdriver.Chrome()
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument('headless')
+    chrome_options.add_argument('no-sandbox')
+    driver = webdriver.Chrome(chrome_options=chrome_options)
     driver.get('https://www.avaloncommunities.com/california/san-francisco-apartments/avalon-at-mission-bay')
 
     time.sleep(5)
